c64aa23f92d0d0ae92f284daaaae7c0513ff4c1b
Stefan Kostoski <72026527+stefankostoski@users.noreply.github.com>
2022-10-11 19:39:21 +0200

    Revert "Api controller created."
    This reverts commit 9c9c1de47453840dfe82a1f20d4f0528afc305c6.

--- NewsAggregator/NewsAggregator.Services/Implementation/CommentService.cs
+++ NewsAggregator/NewsAggregator.Services/Implementation/CommentService.cs
@@ -31,28 +31,15 @@ namespace NewsAggregator.Services.Implementation
             var newComment = user.AddComment(comment.Content, article);
             _commentRepository.Create(newComment);
         }
-        public void Update(CommentDto comment, int commentId, int userId)
+        public void Update(CommentDto comment, int commentId)
         {
-            var user = _userRepository.GetById(userId) ?? throw new UserException(404, "User not found");
             var entity = _commentRepository.GetById(commentId) ?? throw new Exception("Comment not found!");
-
-            if (user.Id != comment.UserId)
-            {
-                throw new Exception("You are not authorized to update this comment!");
-            }
             entity.Update(comment);
             _commentRepository.Update(entity);
         }
-        public void Delete(int id, int userId)
+        public void Delete(int id)
         {
-
-            var user = _userRepository.GetById(userId) ?? throw new UserException(404, "User not found");
             var comment = _commentRepository.GetById(id) ?? throw new Exception("Comment not found!");
-
-            if (user.Id != comment.UserId)
-            {
-                throw new Exception("You are not authorized to update this comment!");
-            }
 
             _commentRepository.Delete(comment);
         }
